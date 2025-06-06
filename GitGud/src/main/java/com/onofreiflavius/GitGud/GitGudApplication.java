aab0f13cb03ca5e304159434f4c4ab9c8871ec52
0nofreiFlav1us <onoflavius@gmail.com>
2025-06-06 10:36:01 +0300

    backend

---
+++ GitGud/src/main/java/com/onofreiflavius/GitGud/GitGudApplication.java
@@ -0,0 +1,13 @@
+package com.onofreiflavius.GitGud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitGudApplication.class, args);
+	}
+
+}
---
+++ GitGud/src/test/java/com/onofreiflavius/GitGud/GitGudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onofreiflavius.GitGud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
